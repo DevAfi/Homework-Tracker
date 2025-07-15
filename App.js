92d9b67f04f203c5afe773b85fa5b1cfbe77d14a
Afonso Carvalho <afonsomiguelcarvalho2006@gmail.com>
2025-07-15 02:49:25 +0100

    Added bare bones of UI

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.taskContainer}>
+        <Text style={styles.taskTitle}>Upcoming tasks</Text>
+
+        <View style={styles.tasks}>
+          {/* HW */}
+          {/* HW */}
+          {/* HW */}
+          {/* HW */}
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#eaecc6',
+  },
+  taskContainer: {
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    flex: 1,
+  },
+  taskTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#2bc0e4',
+  },
+  tasks
+  : {
+
   },
 });
